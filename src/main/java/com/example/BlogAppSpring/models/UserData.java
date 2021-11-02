2e4f06b7f8c5d088354fa1ea8fd77d7321e31769
zsofa <hzsofi12@gmail.com>
2021-11-02 17:31:17 +0100

    connected database

--- src/main/java/com/example/BlogAppSpring/models/UserData.java
+++ src/main/java/com/example/BlogAppSpring/models/UserData.java
@@ -1,9 +1,13 @@
+/*
 package com.example.BlogAppSpring.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @Getter @Setter
 @Entity
@@ -23,6 +27,35 @@ public class UserData {
     private String email;
     private byte[] profilePicture;
 
+    @CreationTimestamp
+    private LocalDateTime regTime;
+
     @Enumerated
     private State state;
+
+    public UserData() {
+    }
+
+    public UserData(long id, String userName,
+                    String password, String email, byte[] profilePicture, State state) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.profilePicture = profilePicture;
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "UserData{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", profilePicture=" + Arrays.toString(profilePicture) +
+                ", state=" + state +
+                '}';
+    }
 }
+*/
